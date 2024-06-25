0728487a3dc098c9fc04115d80fbd7550a70a533
bfidel0 <bfidel0@users.noreply.github.com>
2024-06-25 14:53:29 -0700

    Added backprop and ML model finish along with a few ratios to later improve accuracy

--- nn/nn.py
+++ nn/nn.py
@@ -2,6 +2,9 @@ import yfinance as yf
 import os
 import json
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import precision_score
+import numpy as np
 
 API_PATH = "msft_data.json"
 
@@ -16,4 +19,90 @@ else:
 
     msft_prices.to_json(API_PATH)
 
-print(msft_prices.plot.line(y="Close", use_index=True))
+#print(msft_prices.plot.line(y="Close", use_index=True))
+
+#Determines the actual closing prices
+data = msft_prices[["Close"]]
+data = data.rename(columns= {"Close": "Actual_Close"})
+
+
+# sets our target price indicating if price went up or down
+data["Target"] = msft_prices.rolling(2).apply(lambda x: x.iloc[1] > x.iloc[0])["Close"]
+
+
+#shift the stock price forward one day 
+msft_prev = msft_prices.copy()
+msft_prev = msft_prev.shift(1)
+
+#training data
+predictors = ["Close", "Volume", "Open", 'High', "Low"]
+data = data.join(msft_prev[predictors]).iloc[1:]
+
+#print(data.head(5))
+
+
+'''
+The machine learning model starts here
+'''
+#The min sample split ensures we dont overfit the data
+model = RandomForestClassifier(n_estimators=100, min_samples_split=200, random_state=1)
+
+training_set = data.iloc[:-100]
+test_set = data.iloc[-100:]
+
+model.fit(training_set[predictors], training_set["Target"])
+
+
+#Evaluates the error in our predictions
+preds = model.predict(test_set[predictors])
+preds = pd.Series(preds, index=test_set.index)
+print(precision_score(test_set["Target"], preds))
+
+
+def backtest(data, model, predictors, start=1000, step=750):
+    predictons = []
+
+    for i in range(start, data.shape[0], step):
+        training_set = data.iloc[0:i].copy()
+        test_set = data.iloc[i:(i+step)].copy()
+
+        model.fit(training_set[predictors], training_set["Target"])
+
+        preds = model.predict_proba(test_set[predictors])[:,-1]
+        preds = pd.Series(preds, index=test_set.index)
+        preds[preds > .6] = 1
+        preds[preds <= .6] = 0
+
+        combined = pd.concat({"Target": test_set["Target"], "Predictions": preds}, axis=1)
+
+        predictons.append(combined)
+
+    return pd.concat(predictons)
+
+
+predictions = backtest(data, model, predictors)
+
+print(predictions["Predictions"].value_counts())
+
+print(precision_score(predictions["Target"], predictions["Predictions"]))
+
+
+
+#Improving accuracy functions
+
+weekly_mean = data.rolling(7).mean()
+quarterly_mean = data.rolling(90).mean()
+annual_mean = data.rolling(365).mean()
+weekly_trend = data.shift(1).rolling(7).mean()["Target"]
+
+data["weekly_mean"] = weekly_mean["close"] / data["Close"]
+data["quaretly_mean"] = quarterly_mean["Close"] / data["Close"]
+data["annual_mean"] = annual_mean["Close"] / data["Close"]
+
+data["annual_weekly_mean"] = data["annual_mean"] / data["weekly_mean"]
+data["annual_quarterly_mean"] = data["annual_mean"] / data["quaretly_mean"]
+data["weekly_trend"] = weekly_trend
+
+data["open_close_ratio"] = data["Open"] / data["Close"]
+data["high_close_ratio"] = data["High"] / data["Close"]
+data["low_close_ratio"] = data["Low"] / data["Close"]
